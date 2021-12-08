cc940d4aba2a82796e42daad5d1e1fea142d82e8
moni-tu <turani.monica@gmail.com>
2021-12-08 10:48:58 +0100

    feat: import imported the required modules for use

--- index.js
+++ index.js
@@ -98,3 +98,50 @@ next();
 app.listen(8080, () =>{
     console.log('This app is listening on port 8080.');
   });
+
+// Gets the list of data about ALL movies to the user
+app.get('/movies', (req, res) => {
+  res.status(200).json(movies);
+
+// Gets the data about a single movie, by titles
+
+app.get('/movies/:title', (req, res) => {
+  res.status(200).json(movies.find((movie) =>
+      { return movie.title === req.params.title}));
+  });
+
+// Adds a new movie to our list of movies.
+app.post('/movies/:title', (req, res) => {
+  let newMovie = req.body;
+
+  if (!newMovie.title) {
+    const message = 'Missing title in request body';
+    res.status(400).send(message);
+  } else {
+    newMovie.id = uuid.v4();
+    movies.push(newMovie);
+    res.status(201).send(newMovie);
+  }
+});
+
+// Deletes a movie from our list by title
+app.delete('/movies/:deleteMovie', (req, res) => {
+let movies = movies.find((movie) => { return movie.title === req.params.title });
+
+if (movies) {
+  movies = movies.filter((obj) => { return obj.title !== req.params.title });
+  res.status(201).send('movie ' + req.params.title + ' was deleted.');
+}
+});
+
+// Update the genre of a movie by movie title
+app.put('/movies/:title/:genre', (req, res) => {
+let movie = movies.find((movie) => { return movie.title === req.params.title });
+
+if (movie) {
+  movie.title[req.params.title] = parseInt(req.params.genre);
+  res.status(201).send('Movie ' + req.params.title + ' was assigned a genre of ' + req.params.genre + ' in ' + req.params.title);
+} else {
+  res.status(404).send('Movie with the name ' + req.params.title + ' was not found.');
+}
+})});
