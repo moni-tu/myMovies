97a46f90d89d51236cb5f4f9cfc431f4d20c0bac
moni-tu <turani.monica@gmail.com>
2021-12-22 17:36:45 +0100

    feat: add Userschema and Movieschema
    Also, add models from the schemas.
    Add export code

--- models.js
+++ models.js
@@ -1,1 +1,32 @@
 const mongoose = require('mongoose');
+
+let movieSchema = mongoose.Schema({
+    Title: {type: Sring, required: true},
+    Description: {type: String, required: true},
+    Genre: {
+        Name: String,
+        Description: String
+    },
+    Director: {
+        Name: String,
+        Bio: String
+    },
+    Actors: [String],
+    ImagePath: String,
+    Featured: Boolean
+});
+
+let userSchema = mongoose.Schema({
+    Username: {type: String, required: true},
+    Password: {type: String, require: true},
+    Email:{type: String, require: true},
+    Birthday: Date,
+    FavouriteMovies: [{type: mongoose.Schema.Types.ObjectId, ref: 'Movie'
+    }]
+});
+// this creates collections called db.movies and db.users (plurals and lower case)
+let Movie = mongoose.model('Movie', movieSchema);
+let User = mongoose.model('User', userSchema);
+// need to be imported into index.js
+module.exports.Movie = Movie;
+module.exports.User = User;
