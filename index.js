d88bb9d74a664d35d0051721a0e790c52c54c3d1
moni-tu <turani.monica@gmail.com>
2021-12-24 19:57:05 +0100

    feat: add PUT user info endpoint, add /registration endpoint to POST new user

--- index.js
+++ index.js
@@ -93,7 +93,25 @@ app.get('/director/:name', passport.authenticate("jwt", { session: false }), (re
   Email: String,
   Birthday: Date
 }*/
-app.post('/users', (req, res) => {
+app.post(
+  '/registration', 
+  [
+    check("Username", "Username is required").isLength({ min: 5 }),
+    check(
+      "Username",
+      "Username contains non alphanumeric characters - not allowed."
+    ).isAlphanumeric(),
+    check("Password", "Password is required").not().isEmpty(),
+    check("Email", "Email does not appear to be valid").isEmail(),
+  ],
+(req, res) => {
+  // check the validation object for errors
+  let errors = validationResult(req);
+
+  if (!errors.isEmpty()) {
+    return res.status(422).json({ errors: errors.array() });
+  }
+  let hashedPassword = Users.hashPassword(req.body.Password);
   Users.findOne({ username: req.body.username })
     .then((user) => {
       if (user) {
@@ -142,6 +160,26 @@ app.get('/users', passport.authenticate("jwt", { session: false }), (req, res) =
       res.status(500).send('Error: ' + err);
     });
 });
+// Allow users to update their user information
+app.put(
+  "/users/:username",
+  passport.authenticate("jwt", { session: false }),
+  [
+    check("Username", "Username is required").isLength({ min: 5 }),
+    check(
+      "Username",
+      "Username contains non alphanumeric characters - not allowed."
+    ).isAlphanumeric(),
+    check("Password", "Password is required").not().isEmpty(),
+    check("Email", "Email does not appear to be valid").isEmail(),
+  ],
+  (req, res) => {
+    // check the validation object for errors
+    let errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      return res.status(422).json({ errors: errors.array() });
+    }
 
 // 7. Add a movie to a user's list of favorites
 app.post('/users/:username/favorites/:MovieID', passport.authenticate("jwt", { session: false }), (req, res) => {
