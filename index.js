dddad0c3c43051509abc88bd9b0842ed059e21b2
moni-tu <turani.monica@gmail.com>
2022-05-12 12:08:29 +0200

    feat: add localhost:4200 and mymovie-backend-api.herokuapp in allowed origins

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ app.use(morgan('common'));
 app.use(express.static('public'));
 app.use(bodyParser.json());
 
-let allowedOrigins = ['http://localhost:8080', 'http://testsite.com', 'http://localhost:1234', 'https://my-awesome-movies.netlify.app/'];
+let allowedOrigins = ['http://localhost:8080', 'http://testsite.com', 'http://localhost:1234', 'https://my-awesome-movies.netlify.app/', 'http://localhost:4200/', 'https://mymovie-backend-api.herokuapp.com/'];
 app.use(cors({
   origin: (origin, callback) => {
     if(!origin) return callback(null, true);
